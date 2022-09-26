e0c22e524d909251f3c8aaf57ab1da00961d49f8
tavish-bell <tavish.bell@pm.me>
2022-09-26 16:42:37 -0700

    added beginning of ajax to update DOM

--- static/generate-playlist.js
+++ static/generate-playlist.js
@@ -1,14 +1,14 @@
-// document.querySelector("#user-date").addEventListener("submit", (evt) => {
-//   evt.preventDefault();
+document.querySelector("#user-date").addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-//   const date = evt.target.querySelector("input").value;
+  const date = evt.target.querySelector("input").value;
 
-//   fetch("/api/generate-playlist?" + "date=" + date)
-//     .then((res) => {
-//       res.json();
-//     })
-//     .then((json) => {
-//       // where updating DOM would happen / next steps
-//       console.log(json);
-//     });
-// });
+  fetch("/api/generate-playlist?" + "date=" + date)
+    .then((res) => {
+      res.json();
+    })
+    .then((json) => {
+      // where updating DOM would happen / next steps
+      console.log(json);
+    });
+});
