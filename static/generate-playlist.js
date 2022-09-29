94b3de87291b26c92116e30a24be170b2362b060
tavish-bell <tavish.bell@pm.me>
2022-09-29 13:01:08 -0700

    completed ajax

--- static/generate-playlist.js
+++ static/generate-playlist.js
@@ -1,6 +1,9 @@
 document.querySelector("#user-date").addEventListener("submit", (evt) => {
   evt.preventDefault();
-  // hiding top section, show bottom section--> display=None
+  document.querySelector("#user-date").style.display = "none";
+  document.querySelector("#header-text").style.display = "none";
+  document.querySelector("#musical-messages").style.display = "block";
+
   const formObject = {
     date: document.querySelector("#date").value,
     message: document.querySelector("#message").value,
@@ -11,16 +14,10 @@ document.querySelector("#user-date").addEventListener("submit", (evt) => {
   const url = `/api/generate-playlist?${queryString}`;
 
   fetch(url)
-    // fetching msg data from Playlist table to display
     .then((res) => res.json())
     .then((json) => {
-      // set top div to None, set bottom div to block
       console.log(json);
-      //have url open in new tab with target=blank --> figure out where to put that syntax
       document.querySelector("#finished-playlist").href = json.spotify_url;
-      // revealing href with finished playlist happens here
+      document.querySelector("#finished-playlist").style.display = "block";
     });
 });
-
-//   <!-- style.display set to None (to hide)-->
-// <!-- default style set to None, appear when JS happens-->
