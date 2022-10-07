9f61b1861c6c023236ada26cb9e8df390e658b02
tavish-bell <tavish.bell@pm.me>
2022-10-07 12:31:52 -0700

    fixed bug. all features working!

--- static/generate-playlist.js
+++ static/generate-playlist.js
@@ -1,21 +1,25 @@
 document.querySelector("#user-date").addEventListener("submit", (evt) => {
   evt.preventDefault();
   document.querySelector("#user-date").style.display = "none";
+  console.log(evt);
   document.querySelector("#header-text").style.display = "none";
   document.querySelector("#musical-messages").style.display = "block";
   document.querySelector("#top-div-heading").style.display = "none";
   document.querySelector("#loading-gif").style.display = "block";
   document.querySelector("#top-section").style.display = "none";
-  document.querySelector(".flash-message").style.display = "none";
 
   const formObject = {
     date: document.querySelector("#date").value,
     message: document.querySelector("#message").value,
   };
 
+  console.log(formObject);
+
   const queryString = new URLSearchParams(formObject).toString();
 
   const url = `/api/generate-playlist?${queryString}`;
+
+  console.log(url);
 
   fetch(url)
     .then((res) => res.json())
